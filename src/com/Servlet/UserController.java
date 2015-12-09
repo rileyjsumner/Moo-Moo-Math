5f0aefcac859e182d930c6679830f382bc5d9afc
unknown <samue_schei780@AHS-206LAB-14.ah.isd11>
2015-12-09 16:24:10 -0600

    Added Basics

---
+++ src/com/Servlet/UserController.java
@@ -0,0 +1,46 @@
+package com.Servlet;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class UserController extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private static String INSERT_OR_EDIT = "/user.jsp";
+    private static String LIST_USER = "/listUser.jsp";
+
+    public UserController() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String forward="";
+        String action = request.getParameter("action");
+
+        if (action.equalsIgnoreCase("delete")){
+        
+        }
+
+        RequestDispatcher view = request.getRequestDispatcher(forward);
+        view.forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Date dob = new SimpleDateFormat("MM/dd/yyyy").parse(request.getParameter("dob"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        String userid = request.getParameter("userid");
+        RequestDispatcher view = request.getRequestDispatcher(LIST_USER);
+        view.forward(request, response);
+    }
+}
