91c070db2fc12fa02968bbb871a622e7cc1ad8ce
Riley Sumner <tornadoriley@gmail.com>
2015-12-19 17:36:05 +0000

    correctStatement.java created online with Bitbucket

---
+++ correctStatement.java
@@ -0,0 +1,37 @@
+package Tutorial;
+
+import static Tutorial.RandomNumber.addPoints;
+
+public class correctStatement 
+{
+    public static void correctInt(int x, int y)
+    {
+        if (x == y)
+        {
+            System.out.println("Correct!");
+            addPoints(10);
+        }
+        else
+        {System.out.println("Incorrect. The correct answer is " + x);}
+    }
+    public static void correctDouble(double x2, double y2)
+    {
+        if (x2 == y2)
+        {
+            System.out.println("Correct!");
+            addPoints(10);
+        }
+        else
+        {System.out.println("Incorrect. The correct answer is " + x2);}
+    }
+    public static void correctString(String x1, String y1)
+    {
+        if (x1.equals(y1))
+        {
+            System.out.println("Correct!");
+            addPoints(10);
+        }
+        else
+        {System.out.println("Incorrect. The correct answer is " + x1);}
+    }
+}
